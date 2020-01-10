57329fee33dab9e8b0eef696cd08cc5d1207e153
Paul Kramer <pk@cantamen.de>
2020-01-10 23:19:26 +0100

    added attachments to mirroring

--- src/main/java/listeners/MirrorListener.java
+++ src/main/java/listeners/MirrorListener.java
@@ -1,11 +1,13 @@
 package listeners;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import config.Config;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.GuildChannel;
+import net.dv8tion.jda.api.entities.Message.Attachment;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
@@ -31,8 +33,9 @@ public class MirrorListener extends AbstractListener {
     GuildChannel mirrorGuildChannel = oChannel
         .orElseGet(() -> mirrorGuild.createCopyOfChannel(event.getChannel()).complete());
     TextChannel mirrorChannel = jda.getTextChannelById(mirrorGuildChannel.getId());
-    mirrorChannel
-        .sendMessage("**__" + event.getMember().getEffectiveName() + ":__**\n" + event.getMessage().getContentDisplay())
+    mirrorChannel.sendMessage("**__" + event.getMember().getEffectiveName() + ":__**\n"
+        + event.getMessage().getContentDisplay() + event.getMessage().getAttachments().stream().map(Attachment::getUrl)
+            .map(x -> "\n" + x).collect(Collectors.joining()))
         .queue();
   }
 
