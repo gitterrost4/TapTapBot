d62aa04959abfa0c3777c99ae382bf3e923dada8
Paul Kramer <pk@cantamen.de>
2020-02-21 07:49:36 +0100

    fixes

--- src/main/java/listeners/modtools/BanListener.java
+++ src/main/java/listeners/modtools/BanListener.java
@@ -30,18 +30,18 @@ import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
  */
 public class BanListener extends AbstractMessageListener {
 
-  public Map<String,ChoiceMenu> activeMenus=new HashMap<>();
+  public Map<String, ChoiceMenu> activeMenus = new HashMap<>();
 
   public BanListener(JDA jda) {
-    super(jda,"ban");
+    super(jda, "ban");
     ConnectionHelper.update(
-      "create table if not exists bannedmembers(id INTEGER PRIMARY KEY not null, userid varchar(255) not null, banneduntil text null);");
+        "create table if not exists bannedmembers(id INTEGER PRIMARY KEY not null, userid varchar(255) not null, banneduntil text null);");
     Timer t = new Timer();
-    t.scheduleAtFixedRate(new Unbanner(),10000,10000);
+    t.scheduleAtFixedRate(new Unbanner(), 10000, 10000);
   }
 
   @Override
-  protected void messageReceived(MessageReceivedEvent event,CommandMessage messageContent) {
+  protected void messageReceived(MessageReceivedEvent event, CommandMessage messageContent) {
     event.getMessage().delete().queue();
     if (!guild().getMember(event.getAuthor()).hasPermission(Permission.BAN_MEMBERS)) {
       event.getChannel().sendMessage("***You don't have the right to ban people!***").queue();
@@ -51,50 +51,51 @@ public class BanListener extends AbstractMessageListener {
       event.getChannel().sendMessage("***No user given!***").queue();
       return;
     }
-    
-    Set<Member> possibleMembers=new HashSet<>();
+
+    Set<Member> possibleMembers = new HashSet<>();
     possibleMembers.addAll(event.getMessage().getMentionedMembers());
     possibleMembers.addAll(guild().getMembers().stream()
-      .filter(m -> m.getEffectiveName().toLowerCase().contains(messageContent.getArg(0).get().toLowerCase())
-        || m.getUser().getAsTag().contains(messageContent.getArg(0).get()))
-      .collect(Collectors.toSet()));
-    
-    ChoiceMenuBuilder builder=ChoiceMenu.builder();
+        .filter(m -> m.getEffectiveName().toLowerCase().contains(messageContent.getArg(0).get().toLowerCase())
+            || m.getUser().getAsTag().contains(messageContent.getArg(0).get()))
+        .collect(Collectors.toSet()));
 
-    if(possibleMembers.size()==0) {
-      event.getChannel().sendMessage("***No user found for input "+messageContent.getArg(0).get()+"***").queue();
-      return;      
+    ChoiceMenuBuilder builder = ChoiceMenu.builder();
+
+    if (possibleMembers.size() == 0) {
+      event.getChannel().sendMessage("***No user found for input " + messageContent.getArg(0).get() + "***").queue();
+      return;
     }
 
-    if(possibleMembers.size()>10) {
-      event.getChannel().sendMessage("***Too many users found for input "+messageContent.getArg(0).get()+". Please be more specific.***").queue();
-      return;      
+    if (possibleMembers.size() > 10) {
+      event.getChannel()
+          .sendMessage(
+              "***Too many users found for input " + messageContent.getArg(0).get() + ". Please be more specific.***")
+          .queue();
+      return;
     }
-    Optional<Duration> oDuration=messageContent.getArg(1).map(String::toUpperCase).map(durationString -> {
+    Optional<Duration> oDuration = messageContent.getArg(1).map(String::toUpperCase).map(durationString -> {
       if (durationString.contains("D")) {
-        return durationString.replaceFirst("\\d+D","P$0T");
+        return durationString.replaceFirst("\\d+D", "P$0T");
       } else {
         return "PT" + durationString;
       }
     }).map(Duration::parse);
-    String durationString=oDuration.map(d->" for "+d.toString()
-    .substring(2)
-    .replaceAll("(\\d[HMS])(?!$)", "$1 ")
-    .toLowerCase()).orElse("");
-    possibleMembers.stream().map(m -> new ChoiceMenu.MenuEntry(m.getUser().getAsTag(),m.getId()))
-      .forEach(builder::addEntry);
-    builder.setChoiceHandler(
-      e -> guild().ban(guild().getMemberById(e.getValue()),6).queue(x -> {
-        event.getChannel().sendMessage("***Banned member " + jda.getUserById(e.getValue()).getAsTag() + durationString+"***").queue();
-        oDuration
-          .ifPresent(duration -> ConnectionHelper.update("insert into bannedmembers (userid, muteduntil) VALUES (?,?)",
-            e.getValue(),Instant.now().plus(duration).toString()));
-      }));
+    String durationString = oDuration
+        .map(d -> " for " + d.toString().substring(2).replaceAll("(\\d[HMS])(?!$)", "$1 ").toLowerCase()).orElse("");
+    possibleMembers.stream().map(m -> new ChoiceMenu.MenuEntry(m.getUser().getAsTag(), m.getId()))
+        .forEach(builder::addEntry);
+    builder.setChoiceHandler(e -> guild().ban(guild().getMemberById(e.getValue()), 6).queue(x -> {
+      event.getChannel()
+          .sendMessage("***Banned member " + jda.getUserById(e.getValue()).getAsTag() + durationString + "***").queue();
+      oDuration
+          .ifPresent(duration -> ConnectionHelper.update("insert into bannedmembers (userid, banneduntil) VALUES (?,?)",
+              e.getValue(), Instant.now().plus(duration).toString()));
+    }));
     builder.setTitle("Ban member");
-    builder.setDescription("Choose a member to be banned"+durationString);
+    builder.setDescription("Choose a member to be banned" + durationString);
 
-    ChoiceMenu menu=builder.build();
-    activeMenus.put(menu.display(event.getChannel()),menu);
+    ChoiceMenu menu = builder.build();
+    activeMenus.put(menu.display(event.getChannel()), menu);
   }
 
   @Override
@@ -107,15 +108,15 @@ public class BanListener extends AbstractMessageListener {
       activeMenus.get(event.getMessageId()).handleReaction(event);
     }
   }
-  
-  private class Unbanner extends TimerTask{
+
+  private class Unbanner extends TimerTask {
 
     @Override
     public void run() {
-      List<String> users=ConnectionHelper.getResults("select userid from bannedmembers where banneduntil<?",
-        rs -> rs.getString("userid"),Instant.now().toString());
+      List<String> users = ConnectionHelper.getResults("select userid from bannedmembers where banneduntil<?",
+          rs -> rs.getString("userid"), Instant.now().toString());
       users.forEach(u -> guild().unban(u)
-        .queue(unused -> ConnectionHelper.update("delete from bannedmembers where userid=?",u)));      
+          .queue(unused -> ConnectionHelper.update("delete from bannedmembers where userid=?", u)));
     }
   }
 
