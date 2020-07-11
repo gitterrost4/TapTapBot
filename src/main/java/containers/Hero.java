f229b92075b518e133c05e1c30d540e2cc8a8cb6
Paul Kramer <pk@cantamen.de>
2020-07-11 23:41:24 +0200

    added more stats to the hero

--- src/main/java/containers/Hero.java
+++ src/main/java/containers/Hero.java
@@ -10,20 +10,37 @@ public class Hero {
   public final String name;
   public final String emoji;
   public final String imageUrl;
-  public final String skill1;
-  public final String skill2;
-  public final String skill3;
-  public final String skill4;
-
-  public Hero(String name, String emoji, String imageUrl, String skill1, String skill2, String skill3, String skill4) {
+  public final String skill1Name;
+  public final String skill1Desc;
+  public final String skill2Name;
+  public final String skill2Desc;
+  public final String skill3Name;
+  public final String skill3Desc;
+  public final String skill4Name;
+  public final String skill4Desc;
+  public final Integer maxHp;
+  public final Integer attack;
+  public final Integer speed;
+  public final Integer defense;
+  public Hero(String name, String emoji, String imageUrl, String skill1Name, String skill1Desc, String skill2Name,
+      String skill2Desc, String skill3Name, String skill3Desc, String skill4Name, String skill4Desc, int maxHp,
+      int attack, int speed, int defense) {
     super();
     this.name = name;
     this.emoji = emoji;
     this.imageUrl = imageUrl;
-    this.skill1 = skill1;
-    this.skill2 = skill2;
-    this.skill3 = skill3;
-    this.skill4 = skill4;
+    this.skill1Name = skill1Name;
+    this.skill1Desc = skill1Desc;
+    this.skill2Name = skill2Name;
+    this.skill2Desc = skill2Desc;
+    this.skill3Name = skill3Name;
+    this.skill3Desc = skill3Desc;
+    this.skill4Name = skill4Name;
+    this.skill4Desc = skill4Desc;
+    this.maxHp = maxHp;
+    this.attack = attack;
+    this.speed = speed;
+    this.defense = defense;
   }
 
 }
--- src/main/java/listeners/HeroListener.java
+++ src/main/java/listeners/HeroListener.java
@@ -15,7 +15,7 @@ public class HeroListener extends AbstractMessageListener {
   public HeroListener(JDA jda) {
     super(jda, "hero", "\\|");
     ConnectionHelper.update(
-        "create table if not exists hero (id INTEGER PRIMARY KEY not null, name text, emoji text, imageurl text, skill1 text, skill2 text, skill3 text, skill4 text);");
+        "create table if not exists hero (id INTEGER PRIMARY KEY not null, name text, emoji text, imageurl text, skill1name text, skill1desc text, skill2name text, skill2desc text, skill3name text, skill3desc text, skill4name text, skill4desc text, maxhp integer, attack integer, speed integer, defense integer);");
   }
 
   @Override
@@ -29,10 +29,12 @@ public class HeroListener extends AbstractMessageListener {
         return;
       }
       ConnectionHelper.update(
-          "insert into hero (name, emoji, imageurl, skill1, skill2, skill3, skill4) values (?,?,?,?,?,?,?)",
+          "insert into hero (name, emoji, imageurl, skill1name,skill1desc, skill2name,skill2desc, skill3name, skill3desc, skill4name, skill4desc, maxhp, attack, speed, defense) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
           messageContent.getArgOrThrow(1), messageContent.getArgOrThrow(2), messageContent.getArgOrThrow(3),
           messageContent.getArgOrThrow(4), messageContent.getArgOrThrow(5), messageContent.getArgOrThrow(6),
-          messageContent.getArgOrThrow(7));
+          messageContent.getArgOrThrow(7), messageContent.getArgOrThrow(8), messageContent.getArgOrThrow(9),
+          messageContent.getArgOrThrow(10), messageContent.getArgOrThrow(11), messageContent.getArgOrThrow(12),
+          messageContent.getArgOrThrow(13), messageContent.getArgOrThrow(14), messageContent.getArgOrThrow(15));
       break;
     case "delete":
       if (event.getMember().getRoles().stream()
@@ -45,11 +47,12 @@ public class HeroListener extends AbstractMessageListener {
       break;
     default:
       String heroName = messageContent.getArg(0).get();
-      Optional<Hero> oHero = ConnectionHelper
-          .getFirstResult("select name, emoji,imageurl, skill1,skill2,skill3,skill4 from hero where lower(name)=?",
-              rs -> new Hero(rs.getString("name"), rs.getString("emoji"), rs.getString("imageUrl"),
-                  rs.getString("skill1"), rs.getString("skill2"), rs.getString("skill3"), rs.getString("skill4")),
-              heroName.toLowerCase());
+      Optional<Hero> oHero = ConnectionHelper.getFirstResult(
+          "select name, emoji,imageurl, skill1name,skill1desc,skill2name,skill2desc,skill3name,skill3desc,skill4name,skill4desc,maxhp,attack,speed,defense from hero where lower(name)=?",
+          rs -> new Hero(rs.getString("name"), rs.getString("emoji"), rs.getString("imageUrl"), rs.getString("skill1name"),rs.getString("skill1desc"),
+              rs.getString("skill2name"), rs.getString("skill2desc"), rs.getString("skill3name"), rs.getString("skill3desc"), rs.getString("skill4name"), rs.getString("skill4desc"), rs.getInt("maxhp"),
+              rs.getInt("attack"), rs.getInt("speed"), rs.getInt("defense")),
+          heroName.toLowerCase());
       if (!oHero.isPresent()) {
         event.getChannel().sendMessage("I couldn't find the hero " + heroName + ".").queue();
         return;
@@ -61,9 +64,14 @@ public class HeroListener extends AbstractMessageListener {
                   .setAuthor(hero.name, null,
                       guild().getEmotesByName(hero.emoji, true).stream().findAny().map(em -> em.getImageUrl())
                           .orElse(null))
-                  .addField("Name", hero.name, false).addField("Skill 1", hero.skill1, false)
-                  .addField("Skill 2", hero.skill2, false).addField("Skill 3", hero.skill3, false)
-                  .addField("Skill 4", hero.skill4, false).setImage(hero.imageUrl).build())
+                  .addField("Name", hero.name, false)
+                  .addField("HP", hero.maxHp.toString(), true)
+                  .addField("Attack", hero.attack.toString(), true)
+                  .addField("Speed", hero.speed.toString(), true)
+                  .addField("Defense", hero.defense.toString(), true)
+                  .addField(hero.skill1Name, hero.skill1Desc, false)
+                  .addField(hero.skill2Name, hero.skill2Desc, false).addField(hero.skill3Name, hero.skill3Desc, false)
+                  .addField(hero.skill4Name, hero.skill4Desc, false).setImage(hero.imageUrl).build())
           .queue();
     }
   }
