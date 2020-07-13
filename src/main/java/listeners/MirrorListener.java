65bac9d0cc60eb880f3c6bb3b24bc1a4fb2f46a0
Paul Kramer <pk@cantamen.de>
2020-07-13 13:05:40 +0200

    modified mirror

--- src/main/java/listeners/MirrorListener.java
+++ src/main/java/listeners/MirrorListener.java
@@ -1,9 +1,12 @@
 package listeners;
 
 import java.awt.Color;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import config.Config;
+import database.ConnectionHelper;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
@@ -16,32 +19,26 @@ public class MirrorListener extends AbstractListener {
 
   public MirrorListener(JDA jda) {
     super(jda);
+    ConnectionHelper.update(
+        "create table if not exists mirrors(id INTEGER PRIMARY KEY not null, channelid text not null, mirrorserverid text not null, mirrorchannelid TEXT not null);");
   }
 
   @Override
   protected void guildMessageReceived(GuildMessageReceivedEvent event) {
     super.guildMessageReceived(event);
 
-    if (Config.get("mirror.excludedChannelIds").contains(event.getChannel().getId())) {
-      return;
-    }
+    List<List<String>> mirrors = ConnectionHelper.getResults(
+        "select mirrorserverid, mirrorchannelid from mirrors where channelid=?",
+        rs -> Arrays.asList(rs.getString("mirrorserverid"), rs.getString("mirrorchannelid")),
+        event.getChannel().getId());
 
-    String mirrorServerId = Config.get("mirror.mirrorServerId");
-    Guild mirrorGuild = jda.getGuildById(mirrorServerId);
+    mirrors.stream().forEach(m -> {
+      String mirrorServerId = m.get(0);
+      Guild mirrorGuild = jda.getGuildById(mirrorServerId);
 
-    Optional<GuildChannel> oChannel = mirrorGuild.getChannels().stream()
-        .filter(c -> c.getName().equals(event.getChannel().getName())).findFirst();
-    GuildChannel mirrorGuildChannel = oChannel
-        .orElseGet(() -> mirrorGuild.createCopyOfChannel(event.getChannel()).complete());
-    TextChannel mirrorChannel = jda.getTextChannelById(mirrorGuildChannel.getId());
-    EmbedBuilder embedBuilder = new EmbedBuilder();
-    embedBuilder.setColor(Color.BLACK);
-    embedBuilder.setAuthor(event.getMember().getEffectiveName(), null, event.getMember().getUser().getAvatarUrl());
-    embedBuilder.setDescription(event.getMessage().getContentDisplay());
-    embedBuilder
-        .setImage(event.getMessage().getAttachments().stream().map(Attachment::getUrl).findFirst().orElse(null));
-    mirrorGuild.modifyNickname(mirrorGuild.getSelfMember(), event.getMember().getEffectiveName())
-        .queue(unused -> mirrorChannel.sendMessage(embedBuilder.build()).queue());
+      TextChannel mirrorChannel = mirrorGuild.getTextChannelById(m.get(1));
+      mirrorChannel.sendMessage(event.getMessage().getContentRaw()).queue();
+    });
   }
 
 }
