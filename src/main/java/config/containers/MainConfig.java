ece4a1ad315aaa27fc5046a660f7b3e20dd192f7
Paul Kramer <gitterrost4@gmail.com>
2020-09-24 23:22:34 +0200

    forgot some files

---
+++ src/main/java/config/containers/MainConfig.java
@@ -0,0 +1,43 @@
+package config.containers;
+
+import java.io.IOException;
+import java.util.List;
+
+import config.Config;
+import net.dv8tion.jda.api.JDA;
+
+public class MainConfig {
+  private List<ServerConfig> servers;
+  private ServerConfig defaultConfig;
+
+  public List<ServerConfig> getServers() {
+    return servers;
+  }
+
+  public ServerConfig getDefaultConfig() {
+    return defaultConfig;
+  }
+
+  public void addDefaultServerConfigIfAbsent(String serverId, String serverName, JDA jda) {
+    try {
+      if (!servers.stream().anyMatch(serverConfig -> serverConfig.getServerId().equals(serverId))) {
+        // we don't have a config for this server. We need this check because when
+        // discord is down, this event may be triggered for servers we already joined.
+        System.err.println("Adding Guild " + serverId + "(" + serverName + ")");
+        ServerConfig config = Config.objectMapper().treeToValue(Config.objectMapper().valueToTree(defaultConfig),
+            ServerConfig.class); // this is a dirty, dirty hack... deep-copying the whole object by
+                                 // serializing/deserializing it
+        config.serverId = serverId;
+        config.name = serverName;
+        config.databaseFileName = serverId + ".db";
+        servers.add(config);
+        config.addServerModules(jda);
+        Config.saveConfig();
+
+      }
+    } catch (IOException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+}
