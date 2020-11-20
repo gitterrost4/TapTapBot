747139ea8f34ec9b98cdef8fa6371c05928e06b2
Paul Kramer <pk@cantamen.de>
2020-11-20 14:00:25 +0100

    added activity to display the help command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,8 @@ import config.Config;
 import listeners.BotJoinListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Activity.ActivityType;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
@@ -20,7 +22,7 @@ public class Main extends ListenerAdapter {
   public static void main(String[] args) throws LoginException, InterruptedException {
     Logger logger = LoggerFactory.getLogger(Main.class);
     logger.warn("--------------Starting TapTapBot--------------\n");
-    JDA jda = JDABuilder.createDefault(Config.getToken()).setMemberCachePolicy(MemberCachePolicy.ALL).setChunkingFilter(ChunkingFilter.ALL).enableIntents(GatewayIntent.GUILD_MEMBERS).build().awaitReady();
+    JDA jda = JDABuilder.createDefault(Config.getToken()).setActivity(Activity.playing("Use tap.help for info.")).setMemberCachePolicy(MemberCachePolicy.ALL).setChunkingFilter(ChunkingFilter.ALL).enableIntents(GatewayIntent.GUILD_MEMBERS).build().awaitReady();
     jda.addEventListener(new BotJoinListener(jda)); // Listener for new servers
     Config.getConfig().getServers().stream().forEach(config -> {
       config.addServerModules(jda);
